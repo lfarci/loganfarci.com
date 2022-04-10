521fb33697b999a8222b7150594104d661b271ef
Lofaloa <farci.logan@gmail.com>
2022-04-10 20:36:08 +0200

    Loading sources JSON data into the index page component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,26 @@
 import * as React from "react"
+import { useStaticQuery, graphql } from "gatsby"
 
 const IndexPage = () => {
+
+  const {json: data} = useStaticQuery(graphql`
+    query IndexPageQuery {
+      json {
+        pictureSource
+        pictureAlt
+        role
+        name
+        description
+      }
+    }
+  `);
+
   return (
     <main>
-      <h1>Home Page</h1>
-      <p>This is my website home page</p>
+      <h1>{data.name}</h1>
+      <img alt={data.pictureAlt} src={data.pictureSource}></img>
+      <p>{data.role}</p>
+      <p>{data.description}</p>
     </main>
   )
 }
