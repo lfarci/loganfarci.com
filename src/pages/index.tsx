563062f290586e2a0ceb38df10e41332bf7cef74
Lofaloa <farci.logan@gmail.com>
2022-04-10 20:44:46 +0200

    Fixed JSON data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,25 +3,26 @@ import { useStaticQuery, graphql } from "gatsby"
 
 const IndexPage = () => {
 
-  const {json: data} = useStaticQuery(graphql`
-    query IndexPageQuery {
-      json {
-        pictureSource
-        pictureAlt
-        role
-        name
-        description
-      }
-    }
-  `);
+  // const {json: data} = useStaticQuery(graphql`
+  //   query IndexPageQuery {
+  //     json {
+  //       pictureSource
+  //       pictureAlt
+  //       role
+  //       name
+  //       description
+  //     }
+  //   }
+  // `);
 
   return (
-    <main>
-      <h1>{data.name}</h1>
-      <img alt={data.pictureAlt} src={data.pictureSource}></img>
-      <p>{data.role}</p>
-      <p>{data.description}</p>
-    </main>
+    // <main>
+    //   <h1>{data.name}</h1>
+    //   <img alt={data.pictureAlt} src={data.pictureSource}></img>
+    //   <p>{data.role}</p>
+    //   <p>{data.description}</p>
+    // </main>
+    <p>coucou</p>
   )
 }
 
