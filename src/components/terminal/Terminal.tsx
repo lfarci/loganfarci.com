8799fa16375ed4d94a95dbfa19004fa274ef6142
Logan Farci <farci.logan@gmail.com>
2025-02-23 01:32:17 +0100

    Implement command handling and history management in Terminal component

--- src/components/terminal/Terminal.tsx
+++ src/components/terminal/Terminal.tsx
@@ -1,24 +1,48 @@
 "use client";
 import { useState } from "react";
 import Prompt from "./Prompt";
+import handleCommand from "@/core/Commands";
+import "./Terminal.css"; // Import the CSS file
+
+interface IHistoryItem {
+    className: string;
+    input: string;
+    output: string;
+}
 
 export default function Terminal() {
-    const [output, setOutput] = useState<string[]>([]);
+    const [history, setHistory] = useState<IHistoryItem[]>([]);
 
-    const handleCommandSubmit = (command: string) => {
-        if (command === "clear") {
-            setOutput([]);
+    const clearHistory = () => setHistory([]);
+
+    const appendToHistory = (item: IHistoryItem) => setHistory([...history, item]);
+
+    const handleCommandSubmit = (line: string) => {
+        var command = handleCommand(line);
+
+        console.log(command);
+
+        if (command.hasError) {
+            appendToHistory({ className: "error", input: line, output: command.error ?? "" });
+            return;
+        }
+        
+        if (command.clear) {
+            clearHistory();
             return;
         }
 
-        setOutput([...output, "$ " + command]);
+        appendToHistory({ className: "output", input: line, output: command.output });
     };
 
     return (
         <div style={{ backgroundColor: "black", color: "white", padding: "10px", fontFamily: "monospace" }}>
             <div>
-                {output.map((cmd, index) => (
-                    <div key={index}>{cmd}</div>
+                {history.map((item, index) => (
+                    <div key={index}>
+                        <div>$ {item.input}</div>
+                        <div className={item.className}>{item.output}</div>
+                    </div>
                 ))}
             </div>
             <Prompt prompt="$" onCommandSubmit={handleCommandSubmit} />
