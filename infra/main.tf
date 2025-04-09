50a4e0c38c9487374f733de0bb2af339bb061940
Logan Farci <farci.logan@gmail.com>
2025-04-09 18:20:53 +0200

    fix: remove commented-out DNS CNAME record and custom domain resources from main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -9,19 +9,3 @@ resource "azurerm_static_web_app" "this" {
   location            = azurerm_resource_group.this.location
   sku_tier            = var.static_web_app_sku_tier
 }
-
-# resource "azurerm_dns_cname_record" "this" {
-#   name                = "www.${var.static_web_app_custom_domain}"
-#   zone_name           = azurerm_dns_zone.this.name
-#   resource_group_name = azurerm_resource_group.this.name
-#   ttl                 = 300
-#   record              = azurerm_static_web_app.this.default_host_name
-# }
-
-# resource "azurerm_static_web_app_custom_domain" "this" {
-#   static_web_app_id  = azurerm_static_web_app.this.id
-#   domain_name     = "www.${var.static_web_app_custom_domain}"
-#   validation_type = "cname-delegation"
-
-#   depends_on = [azurerm_dns_cname_record.this]
-# }
