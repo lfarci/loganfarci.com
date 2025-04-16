d246451ca662d55c5d204f82b22e04c34a196800
Logan Farci <farci.logan@gmail.com>
2025-04-16 12:55:08 +0200

    fix: add props to Introduction component

--- src/src/app/page.tsx
+++ src/src/app/page.tsx
@@ -1,5 +1,9 @@
 import Introduction from "@/components/home/Introduction";
 
+const heading = "Hi 👋, I'm Logan Farci";
+const introduction = "Welcome on my personal website! I'm a Passionate and results-driven Software Engineer specializing in Microsoft technologies, with a focus on C#, .NET, Visual Studio, and Azure. With three years of hands-on experience in the public, banking, and energy sectors in Belgium, I've demonstrated expertise in crafting robust solutions and driving digital innovation.";
+const image = "/avatar.png";
+
 export default function Home() {
-  return <Introduction />;
+  return <Introduction heading={heading} introduction={introduction} image={image} />;
 }
--- src/src/components/home/Introduction.tsx
+++ src/src/components/home/Introduction.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import Image from 'next/image';
 
-const Introduction: React.FC = () => {
+interface IntroductionProps {
+    image: string;
+    heading: string;
+    introduction: string;
+}
+
+const Introduction: React.FC<IntroductionProps> = ({ image, heading, introduction }) => {
     return (
         <div className="flex flex-col md:flex-row items-center md:items-start bg-white shadow-md rounded-lg p-4 space-y-4">
             <div className="md:mr-4 space-y-4">
-                <h1 className="text-2xl md:text-4xl">Hi 👋, I&apos;m Logan Farci</h1>
-                <p className="text-lg md:text-xl text-gray-600">
-                    Welcome on my personal website! I&apos;m a Passionate and results-driven Software Engineer specializing in Microsoft technologies, with a focus on C#, .NET, Visual Studio, and Azure. With three years of hands-on experience in the public, banking, and energy sectors in Belgium, I&apos;ve demonstrated expertise in crafting robust solutions and driving digital innovation.
-                </p>
+                <h1 className="text-2xl md:text-4xl">{heading}</h1>
+                <p className="text-lg md:text-xl text-gray-600">{introduction}</p>
             </div>
-            <Image src="/avatar.png" alt="Logan Farci" width={192} height={192} className="rounded-md" />
+            <Image src={image} alt="Logan Farci" width={192} height={192} className="rounded-md" />
         </div>
     );
 };
