bab650d67042ab8ef340b23d6bd6aba3fe32b2f7
Logan Farci <farci.logan@gmail.com>
2025-04-16 19:21:11 +0200

    fix: refactor NavigationBar to include menu items and toggle functionality

--- src/src/components/layout/NavigationBar.tsx
+++ src/src/components/layout/NavigationBar.tsx
@@ -1,16 +1,46 @@
 "use client";
 
 import React from 'react';
-import { Navbar, NavbarBrand } from '@heroui/react';
+import { Link, Navbar, NavbarBrand, NavbarContent, NavbarMenu, NavbarMenuItem, NavbarMenuToggle } from '@heroui/react';
 
 interface NavigationBarProps {
     title: string;
 }
 
-const NavigationBar: React.FC<NavigationBarProps> = ({ title }) => <Navbar isBordered>
-    <NavbarBrand>
-        <p className="font-bold text-inherit">{ title }</p>
-    </NavbarBrand>
-</Navbar>;
+const NavigationBar: React.FC<NavigationBarProps> = ({ title }) => {
+    const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+    const menuItems = [
+        "Home"
+    ];
+
+    return <Navbar isBordered onMenuOpenChange={setIsMenuOpen}>
+        <NavbarContent>
+            <NavbarMenuToggle
+                aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+                className="sm:hidden"
+            />
+            <NavbarBrand>
+                <p className="font-bold text-inherit">{title}</p>
+            </NavbarBrand>
+        </NavbarContent>
+        <NavbarMenu>
+            {menuItems.map((item, index) => (
+                <NavbarMenuItem key={`${item}-${index}`}>
+                    <Link
+                        className="w-full"
+                        color={
+                            index === 2 ? "primary" : index === menuItems.length - 1 ? "danger" : "foreground"
+                        }
+                        href="/"
+                        size="lg"
+                    >
+                        {item}
+                    </Link>
+                </NavbarMenuItem>
+            ))}
+        </NavbarMenu>
+    </Navbar>;
+};
 
 export default NavigationBar;
