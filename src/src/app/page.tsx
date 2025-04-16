2f205cdbda5d4cfed939b0e87c1def171342498b
Logan Farci <farci.logan@gmail.com>
2025-04-16 16:38:16 +0200

    fix: update image handling in Introduction component to use structured image props

--- src/src/app/page.tsx
+++ src/src/app/page.tsx
@@ -2,7 +2,14 @@ import Introduction from "@/components/home/Introduction";
 
 const heading = "Hi 👋, I'm Logan Farci";
 const introduction = "Welcome on my personal website! I'm a Passionate and results-driven Software Engineer specializing in Microsoft technologies, with a focus on C#, .NET, Visual Studio, and Azure. With three years of hands-on experience in the public, banking, and energy sectors in Belgium, I've demonstrated expertise in crafting robust solutions and driving digital innovation.";
-const image = "/avatar.png";
+
+const image = {
+  src: "/avatar.png",
+  alt: "Picture of the author: Logan Farci",
+  width: 1024,
+  height: 1024,
+};
+
 const contacts = [
   {
     name: "LinkedIn",
--- src/src/components/home/Introduction.tsx
+++ src/src/components/home/Introduction.tsx
@@ -6,32 +6,39 @@ interface ContactProps {
     url: string;
 }
 
+interface ImageProps {
+    src: string;
+    alt: string;
+    width: number;
+    height: number;
+}
+
 interface IntroductionProps {
-    image: string;
     heading: string;
     introduction: string;
+    image?: ImageProps;
     contacts?: ContactProps[]
 }
 
 const Contact: React.FC<ContactProps> = ({ name, url }) => <a href={url} target="_blank" rel="noopener noreferrer" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">{ name }</a>;
 
+const Contacts: React.FC<{ contacts: ContactProps[] }> = ({ contacts }) => <div className="flex flex-wrap gap-4">
+    {contacts.map((contact, index) => (
+        <Contact key={index} {...contact} />
+    ))}
+</div>;
+
 const Introduction: React.FC<IntroductionProps> = ({ image, heading, introduction, contacts = [] }) => {
     const showContacts = contacts.length > 0;
 
     return (
-        <div className="flex flex-col md:flex-row items-center md:items-start bg-white shadow-md rounded-lg p-6 space-y-6">
-            <div className="md:mr-6 space-y-6">
-            <h1 className="text-2xl md:text-4xl">{heading}</h1>
-            <p className="text-lg md:text-xl text-gray-600">{introduction}</p>
-            {showContacts && (
-                <div className="flex space-x-6">
-                {contacts.map((contact, index) => (
-                    <Contact key={index} {...contact} />
-                ))}
-                </div>
-            )}
+        <div className="flex flex-col md:flex-row items-center md:items-start bg-white p-6 space-y-6">
+            <div className="md:mr-6 space-y-6 flex-1">
+                <h1 className="text-2xl md:text-4xl">{heading}</h1>
+                <p className="text-lg md:text-xl text-gray-600">{introduction}</p>
+                {showContacts && <Contacts contacts={contacts} />}
             </div>
-            <Image src={image} alt="Logan Farci" width={192} height={192} className="rounded-md" />
+            {image && <Image {...image} className="rounded-md w-full md:w-1/4" />}
         </div>
     );
 };
