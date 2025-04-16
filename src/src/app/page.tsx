562c925980c36138da9083a7ceb73a185135ff37
Logan Farci <farci.logan@gmail.com>
2025-04-16 14:44:43 +0200

    fix: add contacts prop to Introduction component and update layout

--- src/src/app/page.tsx
+++ src/src/app/page.tsx
@@ -3,7 +3,25 @@ import Introduction from "@/components/home/Introduction";
 const heading = "Hi 👋, I'm Logan Farci";
 const introduction = "Welcome on my personal website! I'm a Passionate and results-driven Software Engineer specializing in Microsoft technologies, with a focus on C#, .NET, Visual Studio, and Azure. With three years of hands-on experience in the public, banking, and energy sectors in Belgium, I've demonstrated expertise in crafting robust solutions and driving digital innovation.";
 const image = "/avatar.png";
+const contacts = [
+  {
+    name: "LinkedIn",
+    url: "https://www.linkedin.com/in/lfarci/",
+  },
+  {
+    name: "GitHub",
+    url: "https://github.com/lfarci"
+  },
+  {
+    name: "Bluesky",
+    url: "https://bsky.app/profile/lfarci.bsky.social",
+  },
+  {
+    name: "Email",
+    url: "mailto:logan.farci@outlook.be",
+  }
+];
 
 export default function Home() {
-  return <Introduction heading={heading} introduction={introduction} image={image} />;
+  return <Introduction heading={heading} introduction={introduction} image={image} contacts={contacts}/>;
 }
--- src/src/components/home/Introduction.tsx
+++ src/src/components/home/Introduction.tsx
@@ -1,18 +1,35 @@
 import React from 'react';
 import Image from 'next/image';
 
+interface ContactProps {
+    name: string;
+    url: string;
+}
+
 interface IntroductionProps {
     image: string;
     heading: string;
     introduction: string;
+    contacts?: ContactProps[]
 }
 
-const Introduction: React.FC<IntroductionProps> = ({ image, heading, introduction }) => {
+const Contact: React.FC<ContactProps> = ({ name, url }) => <a href={url} target="_blank" rel="noopener noreferrer" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">{ name }</a>;
+
+const Introduction: React.FC<IntroductionProps> = ({ image, heading, introduction, contacts = [] }) => {
+    const showContacts = contacts.length > 0;
+
     return (
-        <div className="flex flex-col md:flex-row items-center md:items-start bg-white shadow-md rounded-lg p-4 space-y-4">
-            <div className="md:mr-4 space-y-4">
-                <h1 className="text-2xl md:text-4xl">{heading}</h1>
-                <p className="text-lg md:text-xl text-gray-600">{introduction}</p>
+        <div className="flex flex-col md:flex-row items-center md:items-start bg-white shadow-md rounded-lg p-6 space-y-6">
+            <div className="md:mr-6 space-y-6">
+            <h1 className="text-2xl md:text-4xl">{heading}</h1>
+            <p className="text-lg md:text-xl text-gray-600">{introduction}</p>
+            {showContacts && (
+                <div className="flex space-x-6">
+                {contacts.map((contact, index) => (
+                    <Contact key={index} {...contact} />
+                ))}
+                </div>
+            )}
             </div>
             <Image src={image} alt="Logan Farci" width={192} height={192} className="rounded-md" />
         </div>
