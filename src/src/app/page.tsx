2270f433689ea7d7444b73fb617f31171575101e
Logan Farci <farci.logan@gmail.com>
2025-04-18 14:51:06 +0200

    fix: add icons in the interests section

--- src/src/app/page.tsx
+++ src/src/app/page.tsx
@@ -29,6 +29,28 @@ const contacts = [
   }
 ];
 
+import Image from "next/image";
+
+interface IconProps {
+  src: string;
+  alt: string;
+}
+
+const Icon: React.FC<IconProps> = ({ src, alt }) => (
+  <Image 
+    src={src}
+    alt={alt}
+    width={512}
+    height={512}
+    className="text-blue-500 w-full w-1/5"
+  />
+);
+
+const CloudIcon = () => <Icon src="/icons/cloud.png" alt="Cloud icon" />;
+const GearIcon = () => <Icon src="/icons/gear.png" alt="Gear icon" />;;
+const InfinityIcon = () => <Icon src="/icons/infinity.png" alt="Infinity icon" />;;
+const RobotIcon = () => <Icon src="/icons/robot.png" alt="Robot icon" />;
+
 export default function Home() {
   return <div className="flex flex-col space-y-4">
     <Introduction heading={heading} introduction={introduction} image={image} contacts={contacts} />
@@ -37,21 +59,25 @@ export default function Home() {
 
     <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
       <div className="p-4 border rounded shadow-md bg-white flex justify-center items-center flex-col">
+        <GearIcon />
         <h3 className="text-lg font-bold">Backend Development</h3>
         <p className="text-gray-700 text-center">Building robust and scalable backend systems using C#, .NET, and Azure.</p>
       </div>
 
       <div className="p-4 border rounded shadow-md bg-white flex justify-center items-center flex-col">
-        <h3 className="text-lg font-bold">Cloud</h3>
+        <CloudIcon />
+        <h3 className="text-lg font-bold">Azure</h3>
         <p className="text-gray-700 text-center">Leveraging the power of Azure to deliver scalable and efficient solutions.</p>
       </div>
 
       <div className="p-4 border rounded shadow-md bg-white flex justify-center items-center flex-col">
+        <InfinityIcon />
         <h3 className="text-lg font-bold">DevOps</h3>
         <p className="text-gray-700 text-center">Implementing CI/CD pipelines and automating workflows to enhance development efficiency.</p>
       </div>
 
       <div className="p-4 border rounded shadow-md bg-white flex justify-center items-center flex-col">
+        <RobotIcon />
         <h3 className="text-lg font-bold">Artificial Intelligence</h3>
         <p className="text-gray-700 text-center">Exploring machine learning models and AI-driven solutions to solve complex problems.</p>
       </div>
