9bfab888a0247619a8f751ba4179ff4c6c7da5a2
Logan Farci <farci.logan@gmail.com>
2025-04-27 21:30:11 +0200

    fix: simplify component structure in Contacts and Introduction for improved readability

--- src/src/components/home/Contacts.tsx
+++ src/src/components/home/Contacts.tsx
@@ -5,12 +5,10 @@ interface ContactsProps {
     contacts: ExternalLinkProps[];
 }
 
-const Contacts: React.FC<ContactsProps> = ({ contacts }) => (
-    <div className="flex flex-wrap gap-4">
-        {contacts.map((contact, index) => (
-            <ExternalLink key={index} {...contact} />
-        ))}
-    </div>
-);
+const Contacts: React.FC<ContactsProps> = ({ contacts }) => <div className="flex flex-wrap gap-4">
+    {contacts.map((contact, index) => (
+        <ExternalLink key={index} {...contact} />
+    ))}
+</div>;
 
 export default Contacts;
--- src/src/components/home/Introduction.tsx
+++ src/src/components/home/Introduction.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import Image from 'next/image';
-import ExternalLink, { ExternalLinkProps } from '../shared/ExternalLink';
+import { ExternalLinkProps } from '../shared/ExternalLink';
 import GreetingHeading from './GreetingHeading';
-import Contacts from './Contacts'; // Import the extracted Contacts component
+import Contacts from './Contacts';
 
 interface ImageProps {
     src: string;
@@ -17,15 +17,13 @@ interface IntroductionProps {
     contacts?: ExternalLinkProps[];
 }
 
-const Introduction: React.FC<IntroductionProps> = ({ image, introduction, contacts = [] }) => (
-    <div className="flex flex-col md:flex-row items-center pt-8">
-        <div className="md:mr-6 space-y-6 flex-1 flex flex-col justify-center">
-            <GreetingHeading greeting="Hi" name="Logan" />
-            <p className="text">{introduction}</p>
-            {contacts.length > 0 && <Contacts contacts={contacts} />}
-        </div>
-        {image && <Image {...image} className="rounded-xl w-full md:w-1/3 mt-6 md:mt-0" />}
+const Introduction: React.FC<IntroductionProps> = ({ image, introduction, contacts = [] }) => <div className="flex flex-col md:flex-row items-center pt-8">
+    <div className="md:mr-6 space-y-6 flex-1 flex flex-col justify-center">
+        <GreetingHeading greeting="Hi" name="Logan" />
+        <p className="text">{introduction}</p>
+        {contacts.length > 0 && <Contacts contacts={contacts} />}
     </div>
-);
+    {image && <Image {...image} className="rounded-xl w-full md:w-1/3 mt-6 md:mt-0" />}
+</div>;
 
 export default Introduction;
