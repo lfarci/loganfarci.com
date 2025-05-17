589dc5bbcc5d7f246cc5353c468f1885997d8a36
Logan Farci <farci.logan@gmail.com>
2025-05-17 12:24:49 +0200

    fix: remove text-center class from About and HomeSection components for consistency

--- src/src/components/shared/HomeSection.tsx
+++ src/src/components/shared/HomeSection.tsx
@@ -4,7 +4,7 @@ interface HomeSectionProps {
 }
 
 const HomeSection: React.FC<HomeSectionProps> = ({ heading, children }) => <section className="pt-8">
-    <h2 className="text-3xl font-bold text-center mb-4 heading-font">{heading}</h2>
+    <h2 className="text-3xl font-bold mb-4 heading-font">{heading}</h2>
     {children}
 </section>;
 
