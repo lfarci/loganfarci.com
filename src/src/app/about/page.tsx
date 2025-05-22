a455b1e705702d75a773e7586d6862ff1dc766fa
Logan Farci <farci.logan@gmail.com>
2025-05-22 15:33:57 +0200

    Refactor the about page structure and extract the SmallInfoCardsGridSection component

--- src/src/app/about/page.tsx
+++ src/src/app/about/page.tsx
@@ -3,15 +3,25 @@
 import Section from "@/components/shared/Section";
 import { content } from "@/content/home";
 import React from "react";
-import { Certification } from "@/content/types";
 import TextSection from "@/components/shared/TextSection";
 import SmallInfoCard, { SmallInfoCardProps } from "@/components/shared/cards/SmallInfoCard";
-
+import SmallInfoCardsGridSection from "@/components/shared/cards/SmallInfoCardsSection";
+import { Certification } from "@/content/types";
 
 export default function About() {
-  const sortedCredentials = [...content.credentials].sort(
-    (a, b) => (b.relevance ?? 0) - (a.relevance ?? 0)
-  );
+  const certifications: SmallInfoCardProps[] = [...content.credentials]
+    .sort((a, b) => (b.relevance ?? 0) - (a.relevance ?? 0))
+    .map((certification: Certification) => ({
+      image: certification.badge,
+      heading: certification.title,
+      subtitle: certification.issuer,
+      details: [
+        certification.date.toLocaleDateString(undefined, {
+          year: "numeric",
+          month: "long"
+        })
+      ],
+    }));
 
   const bachelor: SmallInfoCardProps = {
     image: content.bachelor.logo,
@@ -24,27 +34,9 @@ export default function About() {
     <div>
       <TextSection heading="About Me" text={content.about} />
       <Section heading="Education">
-          <SmallInfoCard {...bachelor} />
+        <SmallInfoCard {...bachelor} />
       </Section>
-      <Section heading="Certifications">
-        <div className="grid gap-4 mt-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
-          {sortedCredentials.map((credential: Certification, index: number) => (
-            <SmallInfoCard
-              key={index}
-              heading={credential.title}
-              subtitle={credential.issuer}
-              details={[
-                credential.date.toLocaleDateString(undefined, {
-                  year: "numeric",
-                  month: "long"
-                })
-              ]}
-              image={credential.badge}
-              tooltip={credential.title}
-            />
-          ))}
-        </div>
-      </Section>
+      <SmallInfoCardsGridSection heading="Certications" items={certifications} />
     </div>
   );
 }
---
+++ src/src/components/shared/cards/SmallInfoCardsSection.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import SmallInfoCard, { SmallInfoCardProps } from "@/components/shared/cards/SmallInfoCard";
+import Section from "@/components/shared/Section";
+
+type SmallInfoCardsSectionProps = {
+    heading: string;
+    items: SmallInfoCardProps[];
+};
+
+const SmallInfoCardsGridSection: React.FC<SmallInfoCardsSectionProps> = ({ heading, items }) => <Section heading={heading}>
+    <div className="grid gap-4 mt-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+        {items.map((item, index) => <SmallInfoCard key={index} {...item} />)}
+    </div>
+</Section>;
+
+export default SmallInfoCardsGridSection;
