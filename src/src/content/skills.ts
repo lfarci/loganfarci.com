3870279795b521ea70b19017e50da5d44161c27f
Logan Farci <farci.logan@gmail.com>
2025-06-17 22:08:48 +0200

    fix: add skills subfolder in the images directory

--- src/src/content/skills.ts
+++ src/src/content/skills.ts
@@ -13,11 +13,11 @@ export const skillCategories: SkillCategory[] = [
   {
     name: "Backend Development",
     skills: [
-      { name: ".NET", icon: "/images/dotnet.svg" },
-      { name: "C#", icon: "/images/csharp.svg" },
+      { name: ".NET", icon: "/images/skills/dotnet.svg" },
+      { name: "C#", icon: "/images/skills/csharp.svg" },
       { name: "ASP.NET" },
       { name: "Entity Framework" },
-      { name: "Blazor", icon: "/images/blazor.svg" },
+      { name: "Blazor", icon: "/images/skills/blazor.svg" },
       { name: "xUnit" },
       { name: "MSTest" },
     ]
@@ -25,18 +25,18 @@ export const skillCategories: SkillCategory[] = [
   {
     name: "Cloud Computing",
     skills: [
-      { name: "Azure App Service", icon: "/images/azure/app-service.svg" },
-      { name: "Azure Static Web App", icon: "/images/azure/static-web-apps.svg" },
-      { name: "Azure Functions", icon: "/images/azure/function-apps.svg" },
-      { name: "Azure Cosmos DB", icon: "/images/azure/cosmos-db.svg" },
-      { name: "Azure SQL Database", icon: "/images/azure/sql-database.svg" },
-      { name: "Azure Container Registry", icon: "/images/azure/container-registry.svg" },
-      { name: "Azure Container Instances", icon: "/images/azure/container-instances.svg" },
-      { name: "Azure Blob Storage", icon: "/images/azure/blob-storage.svg" },
-      { name: "Azure Communication Services", icon: "/images/azure/communication-services.svg" },
-      { name: "Azure Monitor", icon: "/images/azure/monitor.svg" },
-      { name: "Azure Key Vault", icon: "/images/azure/key-vault.svg" },
-      { name: "Azure API Management", icon: "/images/azure/api-management.svg" },
+      { name: "Azure App Service", icon: "/images/skills/azure/app-service.svg" },
+      { name: "Azure Static Web App", icon: "/images/skills/azure/static-web-apps.svg" },
+      { name: "Azure Functions", icon: "/images/skills/azure/function-apps.svg" },
+      { name: "Azure Cosmos DB", icon: "/images/skills/azure/cosmos-db.svg" },
+      { name: "Azure SQL Database", icon: "/images/skills/azure/sql-database.svg" },
+      { name: "Azure Container Registry", icon: "/images/skills/azure/container-registry.svg" },
+      { name: "Azure Container Instances", icon: "/images/skills/azure/container-instances.svg" },
+      { name: "Azure Blob Storage", icon: "/images/skills/azure/blob-storage.svg" },
+      { name: "Azure Communication Services", icon: "/images/skills/azure/communication-services.svg" },
+      { name: "Azure Monitor", icon: "/images/skills/azure/monitor.svg" },
+      { name: "Azure Key Vault", icon: "/images/skills/azure/key-vault.svg" },
+      { name: "Azure API Management", icon: "/images/skills/azure/api-management.svg" },
     ]
   },
   {
@@ -67,20 +67,20 @@ export const skillCategories: SkillCategory[] = [
   {
     name: "Frontend Development",
     skills: [
-      { name: "TypeScript", icon: "/images/typescript.svg" },
-      { name: "React", icon: "/images/react.svg" },
-      { name: "Next.js", icon: "/images/nextjs.svg" },
-      { name: "Angular", icon: "/images/angular.svg" },
-      { name: "Tailwind CSS", icon: "/images/tailwind.svg" },
+      { name: "TypeScript", icon: "/images/skills/typescript.svg" },
+      { name: "React", icon: "/images/skills/react.svg" },
+      { name: "Next.js", icon: "/images/skills/nextjs.svg" },
+      { name: "Angular", icon: "/images/skills/angular.svg" },
+      { name: "Tailwind CSS", icon: "/images/skills/tailwind.svg" },
     ]
   },
   {
     name: "Development Tools",
     skills: [
-      { name: "Visual Studio", icon: "/images/vs.svg" },
-      { name: "Visual Studio Code", icon: "/images/vscode.svg" },
+      { name: "Visual Studio", icon: "/images/skills/vs.svg" },
+      { name: "Visual Studio Code", icon: "/images/skills/vscode.svg" },
       { name: "Team Foundation Version Control" },
-      { name: "Git", icon: "/images/git.svg" },
+      { name: "Git", icon: "/images/skills/git.svg" },
     ]
   },
 ];
