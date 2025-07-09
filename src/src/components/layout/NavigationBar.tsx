c595d9bb60b0372140499e9dd08196d8b67e3db3
Logan Farci <farci.logan@gmail.com>
2025-07-09 12:58:41 +0200

    fix: Add navigation links to the NavigationBar component

--- src/src/components/layout/NavigationBar.tsx
+++ src/src/components/layout/NavigationBar.tsx
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import { Navbar, NavbarBrand, NavbarContent } from '@heroui/react';
+import Link from 'next/link';
 
 interface NavigationBarProps {
     title: string;
@@ -13,6 +14,11 @@ const NavigationBar: React.FC<NavigationBarProps> = ({ title }) => {
             <NavbarBrand>
                 <p className="text-xl font-thin heading-font">{title}</p>
             </NavbarBrand>
+            <div className="flex space-x-8">
+                <Link href="/" className="hover:text-blue-500 transition-colors">Home</Link>
+                <Link href="/about" className="hover:text-blue-500 transition-colors">About</Link>
+                <Link href="/articles" className="hover:text-blue-500 transition-colors">Articles</Link>
+            </div>
         </NavbarContent>
     </Navbar>;
 };
