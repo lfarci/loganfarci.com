1723fdd396244cfa4192c2ef3b21074b47683e94
Logan Farci <farci.logan@gmail.com>
2026-07-29 12:44:47 +0200

    Use Separator primitive in layout

--- src/src/components/layout/LayoutWrapper.tsx
+++ src/src/components/layout/LayoutWrapper.tsx
@@ -1,6 +1,7 @@
 import NavigationBar from "@/components/layout/NavigationBar";
 import Footer from "@/components/layout/Footer";
 import { contentWidthStyles } from "@/components/layout/contentWidthStyles";
+import { Separator } from "@/components/shared/primitives/Separator";
 
 interface LayoutWrapperProps {
     readonly children: React.ReactNode;
@@ -13,7 +14,7 @@ const LayoutWrapper: React.FC<Readonly<LayoutWrapperProps>> = ({ children, githu
         <>
             <NavigationBar title="Logan Farci" />
             <main className={contentWidthStyles.pageContainer}>{children}</main>
-            <hr className="border-t border-border my-8" />
+            <Separator className="my-8" />
             <Footer githubRepositoryUrl={githubRepositoryUrl} commitHash={commitHash} />
         </>
     );
